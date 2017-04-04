cf10ae9a42459d3678187477e41136f9a9cae30e
git push -u origin <bcmertz@email.wm.edu>
2017-04-04 10:53:11 -0700

    [added] nodejs backend

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,17 @@ var routes = require('./routes');
 
 var app = express();
 
+
+
 // var io=require('socket.io').listen(server);
 
 app.use(express.static('build'));
 
-//middleware configured to use folder 'build' for static? script tags
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use('/', routes);
-
-
-
 
 
 app.listen(3000, function () {
