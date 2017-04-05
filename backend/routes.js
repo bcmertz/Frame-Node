a7caf9231e9c5a0e5f54c9d4a6b19604a80bfdc3
git push -u origin <bcmertz@email.wm.edu>
2017-04-05 10:29:42 -0700

    [fixed] aws credentials in environment

--- backend/routes.js
+++ backend/routes.js
@@ -12,10 +12,10 @@ var mongoose = require('mongoose')
 var Clarifai = require('clarifai');
 
 
-var s3 = new aws.S3(
-  process.env.AWS_ACCESS_KEY_ID,
-  process.env.AWS_SECRET_ACCESS_KEY
-);
+var s3 = new aws.S3({
+  "accessKeyId" : process.env.AWS_ACCESS_KEY_ID,
+  "secretAccessKey" : process.env.AWS_SECRET_ACCESS_KEY
+});
 var bucketParams = {Bucket: 'code-testing'};
 
 var app = express();
