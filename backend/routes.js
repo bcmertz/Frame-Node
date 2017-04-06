0e50fa6c18ff0e24136c5e8a662b7312220754b6
git push -u origin <bcmertz@email.wm.edu>
2017-04-06 10:50:30 -0700

    f

--- backend/routes.js
+++ backend/routes.js
@@ -60,15 +60,18 @@ var postToPython = function (data) {
 router.post('/upload', function (req, res) {
   var tempPath = req.files.photo
   var targetPath = path.resolve(__dirname, './uploadedpics/pic.jpg');
-  console.log('req.files', req.files.photo);
+  console.log('req.files:', req.files.photo, 'type:', typeof(tempPath));
+  console.log('targetPath:', targetPath)
   // fs.createReadStream('file.json').pipe(request.put('http://mysite.com/obj.json'))  //use later to pipe to python server if wanted, prolly not tho cause aws is easier
   // fs.rename(tempPath, targetPath)
   // request(tempPath).pipe(fs.createWriteStream(targetPath))  //probably better than what I currently have
   var uploadedPhoto = req.files.photo;
   uploadedPhoto.mv(targetPath, function(err) {
     if (err) {
+      console.log('err:', err)
       res.status(500).send(err);
     }
+    console.log('here:')
   })
   .then(()=>{
     console.log('image uploaded, saving to aws')
